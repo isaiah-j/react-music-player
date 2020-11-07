8dbe49c2d789fde58d348b62fc4888a058754700
Isaiah <greatpup1940@gmail.com>
2020-11-07 00:42:51 -0500

    Added basic formatting to the song player

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import logo from './logo.svg';
 import Player from './components/Player'
 import Song from './components/Song'
 
-import './App.css';
+import './styles/app.scss';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Player />
       <Song/>
+      <Player />
     </div>
   );
 }
